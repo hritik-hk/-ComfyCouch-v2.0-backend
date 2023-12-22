064429f61273bd3a01ba4c7022b8559a3ca8f6ea
hritik-hk <kumar.hritik4w@gmail.com>
2023-12-22 06:55:48 +0530

    modified cart schema/model, updated cart routes callbacks

--- controller/cart.js
+++ controller/cart.js
@@ -1,10 +1,9 @@
 const { Cart } = require("../model/cart");
 
 exports.fetchCartByUser = async (req, res) => {
-  console.log(req.params)
   const { id } = req.params;
   try {
-    const cartItems = await Cart.find({ user: id }).populate("productVariant");
+    const cartItems = await Cart.find({ userID: id });
     res.status(200).json(cartItems);
   } catch (err) {
     res.status(400).json(err);
@@ -15,8 +14,7 @@ exports.addToCart = async (req, res) => {
   const cart = new Cart({ ...req.body });
   try {
     const doc = await cart.save();
-    const result = await doc.populate("productVariant");
-    res.status(201).json(result);
+    res.status(201).json(doc);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -38,9 +36,8 @@ exports.updateCart = async (req, res) => {
     const cart = await Cart.findByIdAndUpdate(id, req.body, {
       new: true,
     });
-    const result = await cart.populate("product").populate("variant");
 
-    res.status(200).json(result);
+    res.status(200).json(cart);
   } catch (err) {
     res.status(400).json(err);
   }
--- model/cart.js
+++ model/cart.js
@@ -1,23 +1,28 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const cartSchema = new Schema({
-    quantity: { type: Number, required: true },
-    productVariant: { type: Schema.Types.ObjectId, ref: 'Variant', required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  title: { type: String, required: true },
+  productID: { type: Schema.Types.ObjectId, required: true },
+  variantID: { type: Schema.Types.ObjectId, required: true },
+  thumbnail: { type: String, required: true },
+  color: { type: String, required: true },
+  price: { type: Number, required: true },
+  brand: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  userID: { type: Schema.Types.ObjectId, required: true },
 });
 
+const virtual = cartSchema.virtual("id");
+virtual.get(function () {
+  return this._id;
+});
+cartSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
 
-const virtual  = cartSchema.virtual('id');
-virtual.get(function(){
-    return this._id;
-})
-cartSchema.set('toJSON',{
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc,ret) { delete ret._id}
-})
-
-
-exports.Cart = mongoose.model('Cart',cartSchema);
+exports.Cart = mongoose.model("Cart", cartSchema);
