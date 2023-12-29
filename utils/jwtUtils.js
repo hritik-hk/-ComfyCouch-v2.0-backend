3be8964a95ee180e3bb4247fcca5b90d1ac95ea0
hritik-hk <kumar.hritik4w@gmail.com>
2023-12-30 04:03:34 +0530

    added issue jwt function

---
+++ utils/jwtUtils.js
@@ -0,0 +1,28 @@
+const jsonwebtoken = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const pathToKey = path.join(__dirname, '..', 'id_rsa_priv.pem');
+const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+/**
+ The user object is needed to set the JWT `sub` payload property equal to user._id
+ */
+function issueJWT(user) {
+  const _id = user._id;
+
+  const expiresIn = process.env.expiresIn;
+
+  const payload = {
+    sub: _id
+  };
+
+  const signedToken = jsonwebtoken.sign(payload, PRIV_KEY, { expiresIn: expiresIn, algorithm: 'RS256' });
+
+  return {
+    token: "Bearer " + signedToken,
+    expires: expiresIn
+  }
+}
+
+module.exports.issueJWT = issueJWT;
